b9f9836ea580143ae3a6c60e54dcceb8956c059b
joslangarica <joslangarica@gmail.com>
2023-06-23 08:04:12 -0600

    second commit, including initial frontend prototype. Signup, Login, Profile has been created

--- routes/main.js
+++ routes/main.js
@@ -1,24 +1,123 @@
-  // The main.js file of PixelPulse
+// Import necessary modules
+const bcrypt = require('bcrypt');
+
+// The main.js file of PixelPulse
 module.exports = function(app) {
-    app.get("/",function(req, res){
-        res.render("index.html")
+  app.get("/", function(req, res) {
+    res.render("index.html");
+  });
+
+  // Signup route
+  app.get("/signup", function(req, res) {
+    res.render("signup.html");
+  });
+
+  app.post("/signup", function(req, res) {
+    // Get form data
+    let username = req.body.username;
+    let email = req.body.email;
+    let role = req.body.role;
+    
+    // Validate data here
+
+    // Hash password
+    const saltRounds = 10;
+    bcrypt.hash(req.body.password, saltRounds, function(err, hashedPassword) {
+      if (err) {
+        res.status(500).send('Error in password hashing.');
+        return;
+      }
+      
+      // Prepare SQL insert statement
+      let sql = "INSERT INTO users (username, email, password, role) VALUES ?";
+      let values = [
+          [username, email, hashedPassword, role]
+      ];
+      
+      // Execute query
+      db.query(sql, [values], function(err, result) {
+        if(err) {
+          // Handle error
+          res.status(500).send('Error in user registration.');
+        } else {
+          // User registered successfully
+          res.status(200).send('User registration successful.');
+        }
+      });
     });
+  });
 
-    app.get("/search",function(req, res) {
-        res.render("search.html");
+  // Login route
+  app.get("/login", function(req, res) {
+    res.render("login.html");
+  });
+
+  app.post("/login", function(req, res) {
+    // Get form data
+    let email = req.body.email;
+    
+    // Prepare SQL select statement
+    let sql = "SELECT * FROM users WHERE email = ?";
+    
+    // Execute query
+    db.query(sql, [email], function(err, results) {
+      if(err) {
+        // Handle error
+        res.status(500).send('Error during login.');
+      } else {
+        if(results.length > 0){
+          // User exists
+          bcrypt.compare(req.body.password, results[0].password, function(err, result) {
+            if(err) {
+              res.status(500).send('Error during password comparison.');
+              return;
+            }
+            if(result){
+              // User login successful
+              res.render("profile.html", { user: results[0] })
+              //res.status(200).send('User login successful.');
+
+            } else {
+              // Password is incorrect
+              res.status(401).send('Incorrect password.');
+            }
+          });
+        } else {
+          // User does not exist
+          res.status(404).send('User not found.');
+        }
+      }
     });
+  });
 
-    app.get("/search-result", function (req, res) {
-        //searching in the database
-        res.send("This is the keyword you entered: " + req.query.keyword + "<br>" + "This is theresult of the search:");
-       });
 
-    //route for register action
-    app.get("/register", function (req,res) {
-        res.render("register.html");
-       });
-       app.post("/registered", function (req,res) {
-        // saving data in database
-        res.send("Hello "+ req.body.first + " "+ req.body.last +", you are now registered!");
-       });
-   }
+ // Profile route
+  // Profile route
+app.get("/profile/:id", function(req, res) {
+  const userId = req.params.id;
+
+  // Prepare SQL select statement
+  let sql = "SELECT * FROM users WHERE id = ?";
+
+  // Execute query
+  db.query(sql, [userId], function(err, results) {
+    if(err) {
+      // Handle error
+      res.status(500).send('Error during fetching user data.');
+    } else {
+      if(results.length > 0) {
+        // User exists, render profile page with user data
+        res.render("profile.html", { user: results[0] });
+      } else {
+        // User does not exist
+        res.status(404).send('User not found.');
+      }
+    }
+  });
+});
+
+
+
+
+
+}
